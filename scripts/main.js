82ac351ee66ad4ba11e2dbd7f278f0664b123965
laurailona <clarkelaura92@gmail.com>
2018-03-16 10:44:54 +0100

    Add basic functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 typeWriterEffect();
+
+/* Show-Hide NAVIGATION */
+var visible = false;
+var navButton = document.getElementById("show-hide-button");
+var topNav = document.getElementById("top-nav");
+function showHide() {
+    if(visible) {
+      topNav.classList.remove("show-nav");
+      visible = false;
+    }
+    else {
+      topNav.classList.add("show-nav");
+      visible = true;
+    }
+}
+navButton.addEventListener("click", showHide);
